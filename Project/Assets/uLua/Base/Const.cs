8b026aa9ca1266e4b9580ba7c589a780e6de4d55
woodpecker_px <woodpecker.px@qq.com>
2016-06-02 23:58:51 +0800

    Add Project

---
+++ Project/Assets/uLua/Base/Const.cs
@@ -0,0 +1,8 @@
+﻿
+public class Const {   
+    public static bool UsePbc = false;                           //PBC
+    public static bool UseLpeg = false;                          //lpeg
+    public static bool UsePbLua = false;                         //Protobuff-lua-gen
+    public static bool UseCJson = false;                         //CJson
+    public static bool UseSQLite = false;                        //SQLite
+}
---
+++ Project/Assets/uLua/Base/Util.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System;
+using System.Text;
+using System.Collections;
+using System.Reflection;
+using System.IO;
+
+public class Util {
+    /// <summary>
+    /// 取得Lua路径
+    /// </summary>
+    public static string LuaPath(string name) {
+        string path = Application.dataPath + "/";
+        string lowerName = name.ToLower();
+        if (lowerName.EndsWith(".lua")) {
+            return path + "lua/" + name;
+        }
+        return path + "lua/" + name + ".lua";
+    }
+
+    public static void Log(string str) {
+        Debug.Log(str);
+    }
+
+    public static void LogWarning(string str) {
+        Debug.LogWarning(str);
+    }
+
+    public static void LogError(string str) {
+        Debug.LogError(str); 
+    }
+}
